a9e43cc73750e7944f4df280c3108f5b740a4041
Zhengquan Yang <yangzhengquan@gmail.com>
2011-08-09 11:48:20 +0800

    add the backup files (contains all backup & incremental backup)

--- backup_cron.sh
+++ backup_cron.sh
@@ -4,23 +4,35 @@
 #System: Ubuntu 10.04
 #Author: yangkit
 #Email:  yangzhengquan@gmail.com
-#Desc:   a script to configure system,including config of mysql & redis & nginx and som cron tasks.
+#Desc:   a shell script to make the backup jobs be cron tasks 
 ###END INFO
 ##root 
 set -e
+##Global Settings
+PATH=/usr/sbin:/usr/bin:/sbin:/bin
+MYSQL_UPDATE_VOTES="mysql uroot -e \"update users set vote_times_per_day = 30\""
+MYSQL_BACKUP_SCRIPT="./backup_scripts/mysql_back.sh"
+MYSQL_PATH="/usr/bin/mysql"
+CRON_CONFIG_PATH="/etc/crontab"
+LOG_PATH="/var/log/backup.log"
+
 if [ `id -u` -ne 0 ];	then
 	exit 1;
 fi
-PATH=/usr/sbin:/usr/bin:/sbin:/bin
-CRON_MYSQL_COMMAND="mysql uroot -e \"update users set vote_times_per_day = 30\""
-MYSQL_PATH="/usr/bin/mysql"
-CRON_CONFIG_PATH="/etc/crontab"
-LOG_PATH="/var/log/messages"
+function error_prompt ()
+{
+	echo "Error:$1!"
+	exit 1
+}
 ##add cron task
 echo "[+]Writing cron jobs into ${CRON_CONFIG_PATH}..."
+echo "[+]Add the update task on vote_times_per_day..."
 cat <<END_OF >>$CRON_CONFIG_PATH
 ##cron task ,log info when occured error
-0 0 * * * root 	$CRON_MYSQL_COMMAND || ( echo "\$(date) \$(hostname) ${CRON_MYSQL_COMMAND} failed!" >> $LOG_PATH)
+0 0 * * * root 	$MYSQL_UPDATE_VOTES || ( echo "\$(date) \$(hostname) ${MYSQL_UPDATE_VOTES} failed!" >> $LOG_PATH)
 END_OF
+
+##add mysql backup task
+
 echo "Complete!"
 exit 0
---
+++ backup_scripts/conf_backup_all.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+###BEGIN INFO
+#System: Ubuntu 10.04
+#Author: yangkit
+#Email:  yangzhengquan@gmail.com
+#Desc: a script to backup the key configure file using full backup
+###END INFO
