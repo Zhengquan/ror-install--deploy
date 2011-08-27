06b20c8ec39a10ad10615833196180144a25cf50
vigoss <vigoss@luexiao-server.(none)>
2011-08-27 21:00:12 +0800

    delete some files & add the xtrabackup auto-install script

--- backup_scripts/conf_backup_all.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-###BEGIN INFO
-#System: Ubuntu 10.04
-#Author: yangkit
-#Email:  yangzhengquan@gmail.com
-#Desc: a script to backup the key configure file using full backup
-###END INFO
